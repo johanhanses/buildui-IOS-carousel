61b556b97915ed988c58295250e4b2b7b89d9e3e
Johan Hanses <johanhanses@gmail.com>
2023-01-08 12:13:10 +0100

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
+import { AnimatePresence, motion, MotionConfig } from 'framer-motion'
 import { useState } from 'react'
+import useKeypress from 'react-use-keypress'
 
 const images = [
   '/images/1.jpeg',
@@ -8,41 +10,127 @@ const images = [
   '/images/3.jpeg',
   '/images/4.jpeg',
   '/images/5.jpeg',
-  '/images/6.jpeg'
+  '/images/6.jpeg',
+  '/images/1.jpeg?1',
+  '/images/2.jpeg?1',
+  '/images/3.jpeg?1',
+  '/images/4.jpeg?1',
+  '/images/5.jpeg?1',
+  '/images/6.jpeg?1',
+  '/images/1.jpeg?2',
+  '/images/2.jpeg?2',
+  '/images/3.jpeg?2',
+  '/images/4.jpeg?2',
+  '/images/5.jpeg?2',
+  '/images/6.jpeg?2'
 ]
+
+const collapsedAspectRatio = 1 / 3
+const fullAspectRatio = 3 / 2
+const margin = 12
+const gap = 2
 
 export default function Page() {
   const [index, setIndex] = useState(0)
 
+  useKeypress('ArrowRight', () => {
+    if (index < images.length - 1) setIndex(index + 1)
+  })
+
+  useKeypress('ArrowLeft', () => {
+    if (index > 0) setIndex(index - 1)
+  })
+
   return (
-    <div className="h-full min-h-screen bg-black">
-      <div className="mx-auto flex h-full max-w-7xl flex-col items-center">
-        <div className="relative">
-          <img
-            src={images[index]}
-            className="aspect-[3/2] object-cover"
-            alt="hej"
-          />
-
-          {index > 0 && (
-            <button
-              className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white/60 transition hover:bg-white/80"
-              onClick={() => setIndex(index - 1)}
+    <MotionConfig transition={{ duration: 1.2, ease: [0.32, 0.72, 0, 1] }}>
+      <div className="h-full min-h-screen bg-black">
+        <div className="mx-auto flex h-full max-w-7xl flex-col items-center">
+          <div className="relative overflow-hidden">
+            <motion.div
+              animate={{ x: `-${index * 100}%` }}
+              className="flex"
             >
-              <ChevronLeftIcon className="h-6 w-6" />
-            </button>
-          )}
+              {images.map((image, i) => (
+                <motion.img
+                  key={image}
+                  src={image}
+                  className="aspect-[3/2] object-cover"
+                  animate={{ opacity: i === index ? 1 : 0.3 }}
+                  alt="image in carousel"
+                />
+              ))}
+            </motion.div>
+            <AnimatePresence initial={false}>
+              {index > 0 && (
+                <motion.button
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.7 }}
+                  exit={{ opacity: 0, pointerEvents: 'none' }}
+                  whileHover={{ opacity: 1 }}
+                  className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                  onClick={() => setIndex(index - 1)}
+                >
+                  <ChevronLeftIcon className="h-6 w-6" />
+                </motion.button>
+              )}
+            </AnimatePresence>
 
-          {index + 1 < images.length && (
-            <button
-              className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white/60 transition hover:bg-white/80"
-              onClick={() => setIndex(index + 1)}
+            <AnimatePresence initial={false}>
+              {index + 1 < images.length && (
+                <motion.button
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.7 }}
+                  exit={{ opacity: 0, pointerEvents: 'none' }}
+                  whileHover={{ opacity: 1 }}
+                  className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                  onClick={() => setIndex(index + 1)}
+                >
+                  <ChevronRightIcon className="h-6 w-6" />
+                </motion.button>
+              )}
+            </AnimatePresence>
+          </div>
+          <div className="absolute inset-x-0 bottom-6 flex h-24 justify-center overflow-hidden">
+            <motion.div
+              initial={false}
+              animate={{ x: `-${index * 100 * (collapsedAspectRatio / fullAspectRatio) + margin + index * gap}%` }}
+              style={{ aspectRatio: fullAspectRatio, gap: `${gap}%` }}
+              className="flex"
             >
-              <ChevronRightIcon className="h-6 w-6" />
-            </button>
-          )}
+              {images.map((image, i) => (
+                <motion.button
+                  key={image}
+                  initial={false}
+                  onClick={() => setIndex(i)}
+                  whileHover={{ opacity: 1 }}
+                  animate={i === index ? 'active' : 'inActive'}
+                  variants={{
+                    active: {
+                      aspectRatio: fullAspectRatio,
+                      marginLeft: `${margin}%`,
+                      marginRight: `${margin}%`,
+                      opacity: 1
+                    },
+                    inActive: {
+                      aspectRatio: collapsedAspectRatio,
+                      marginLeft: 0,
+                      marginRight: 0,
+                      opacity: 0.5
+                    }
+                  }}
+                  className="shrink-0"
+                >
+                  <img
+                    src={image}
+                    className="h-full object-cover"
+                    alt="hej"
+                  />
+                </motion.button>
+              ))}
+            </motion.div>
+          </div>
         </div>
       </div>
-    </div>
+    </MotionConfig>
   )
 }
